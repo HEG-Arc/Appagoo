553bead4d9d85ddf0119105e591d9a3135add74e
Cédric Gaspoz <cedric@gaspoz-fleiner.com>
2015-01-06 22:21:46 +0100

    Migrated to Foundation

--- appagoo/config/common.py
+++ appagoo/config/common.py
@@ -111,7 +111,7 @@ class Common(Configuration):
 
     # DATABASE CONFIGURATION
     # See: https://docs.djangoproject.com/en/dev/ref/settings/#databases
-    DATABASES = values.DatabaseURLValue('postgres://localhost/appagoo')
+    DATABASES = values.DatabaseURLValue('postgres://appagoo:appagoo@localhost/appagoo')
     # END DATABASE CONFIGURATION
 
     # CACHING
